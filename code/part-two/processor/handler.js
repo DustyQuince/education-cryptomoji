e972751c659aa95f2999df73f0757dbed36cccec
John Otwell <john.otwell@protonmail.com>
2020-02-20 00:27:05 -0500

    Implemented handler logic for createCollection

--- code/part-two/processor/handler.js
+++ code/part-two/processor/handler.js
@@ -1,13 +1,18 @@
-'use strict';
+"use strict";
 
-const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
-const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
-const { decode } = require('./services/encoding');
+const { TransactionHandler } = require("sawtooth-sdk/processor/handler");
+const { InvalidTransaction } = require("sawtooth-sdk/processor/exceptions");
+const { decode, encode } = require("./services/encoding");
+const {
+  getCollectionAddress,
+  getMojiAddress
+} = require("./services/addressing.js");
+const getPrng = require("./services/prng.js");
+const { createHash } = require("crypto");
 
-
-const FAMILY_NAME = 'cryptomoji';
-const FAMILY_VERSION = '0.1';
-const NAMESPACE = '5f4d76';
+const FAMILY_NAME = "cryptomoji";
+const FAMILY_VERSION = "0.1";
+const NAMESPACE = "5f4d76";
 
 /**
  * A Cryptomoji specific version of a Hyperledger Sawtooth Transaction Handler.
@@ -18,9 +23,9 @@ class MojiHandler extends TransactionHandler {
    * validator, declaring which family name, versions, and namespaces it
    * expects to handle. We'll fill this one in for you.
    */
-  constructor () {
-    console.log('Initializing cryptomoji handler with namespace:', NAMESPACE);
-    super(FAMILY_NAME, [ FAMILY_VERSION ], [ NAMESPACE ]);
+  constructor() {
+    console.log("Initializing cryptomoji handler with namespace:", NAMESPACE);
+    super(FAMILY_NAME, [FAMILY_VERSION], [NAMESPACE]);
   }
 
   /**
@@ -46,10 +51,76 @@ class MojiHandler extends TransactionHandler {
    *   - context.deleteState(addresses): deletes the state for the passed
    *     array of state addresses. Only needed if attempting the extra credit.
    */
-  apply (txn, context) {
-    // Enter your solution here
-    // (start by decoding your payload and checking which action it has)
+  apply(txn, context) {
+    const signer = txn.header.signerPublicKey;
+    const payload = decode(txn.payload);
+    switch (payload.action) {
+      case "CREATE_COLLECTION":
+        const addr = getCollectionAddress(signer);
+        return context
+          .getState([addr])
+          .then(state => {
+            if (state[addr].length !== 0) {
+              throw new Error("This address already has a collection.");
+            }
+          })
+          .then(() => {
+            const update = {};
+            const stateResources = {
+              key: signer,
+              moji: this.makeMoji(signer, context, txn.signature, null, null, 3)
+            };
+            update[addr] = encode(stateResources);
+            return context.setState(update);
+          })
+          .catch(err => {
+            throw new InvalidTransaction(err);
+          });
+        break;
 
+      case "SELECT_SIRE":
+        break;
+
+      case "BREED_MOJI":
+        break;
+      default:
+        break;
+    }
+  }
+
+  makeMoji(owner, context, sig, sire = null, breeder = null, numOffspring = 1) {
+    const mojiAddresses = [];
+
+    let seed;
+    if (sire !== null && breeder !== null) {
+      seed = sig + sire + breeder;
+    } else {
+      seed = sig;
+    }
+
+    const prng = getPrng(seed);
+    for (let i = 0; i < numOffspring; i++) {
+      // set up a new moji object
+      let moji = {
+        dna: createHash("sha512")
+          .update(prng(100000000).toString())
+          .digest("hex")
+          .slice(0, 36),
+        owner: owner,
+        breeder: breeder,
+        sire: sire,
+        bred: [],
+        sired: []
+      };
+      // update state with new moji
+      let addr = getMojiAddress(moji.owner, moji.dna);
+      mojiAddresses.push(addr);
+      let update = {};
+      update[addr] = encode(moji);
+      context.setState(update);
+    }
+
+    return mojiAddresses;
   }
 }
 
